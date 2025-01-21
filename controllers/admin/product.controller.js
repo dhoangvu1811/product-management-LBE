625060108e6e20328fa66a20439a9eb292c13459
Đinh Hoàng Vũ <vudh.22itb@vku.udn.vn>
2025-01-21 21:42:53 +0700

    chức năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -39,10 +39,22 @@ module.exports.index = async (req, res) => {
     if (req.query.status) {
         find.status = req.query.status;
     }
+
+    let keyword = '';
+
+    if (req.query.keyword) {
+        keyword = req.query.keyword;
+
+        // "i" không phân biệt chữ hoa chữ thường
+        const regex = new RegExp(keyword, 'i');
+        find.title = regex;
+    }
+
     const products = await Product.find(find);
     res.render('admin/pages/products/index', {
         titlePage: 'Trang sản phẩm',
         products: products,
         filterStatus,
+        keyword: keyword,
     });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -19,3 +19,25 @@ if (buttonStatus.length > 0) {
     });
 }
 /* End button status */
+
+/* Form search */
+
+const formSearch = document.querySelector('#form-search');
+if (formSearch) {
+    //phân tích url
+    let url = new URL(window.location.href);
+
+    formSearch.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const keyword = e.target.elements.keyword.value;
+
+        if (keyword) {
+            url.searchParams.set('keyword', keyword);
+        } else {
+            url.searchParams.delete('keyword');
+        }
+        window.location.href = url.href;
+    });
+}
+
+/* End form search */
