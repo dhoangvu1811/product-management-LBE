5e92654f4dd775e21cd3cb880a056451238a3869
Đinh Hoàng Vũ <vudh.22itb@vku.udn.vn>
2025-01-21 17:44:19 +0700

    lấy sp ra giao diện admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,13 @@
+const Product = require('../../model/product.model');
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false,
+    });
+    console.log(products);
     res.render('admin/pages/products/index', {
         titlePage: 'Trang sản phẩm',
+        products: products,
     });
 };
