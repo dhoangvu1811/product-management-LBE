3ff2b6efe766a850a08ff3440325b7d338f26e86
Đinh Hoàng Vũ <vudh.22itb@vku.udn.vn>
2025-01-22 18:04:55 +0700

    tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -15,17 +15,41 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
+    //tìm kiếm
     const objectSearch = searchHelper(req.query);
 
     if (objectSearch.regex) {
         find.title = objectSearch.regex;
     }
 
-    const products = await Product.find(find);
+    /* pagnation */
+    let objectPagination = {
+        currentPage: 1,
+        limitItems: 4,
+    };
+
+    if (req.query.page) {
+        objectPagination.currentPage = parseInt(req.query.page);
+    }
+
+    objectPagination.skip =
+        (objectPagination.currentPage - 1) * objectPagination.limitItems;
+
+    const countProducts = await Product.countDocuments(find);
+
+    const totalPage = Math.ceil(countProducts / objectPagination.limitItems);
+
+    objectPagination.totalPage = totalPage;
+    /* End pagination */
+
+    const products = await Product.find(find)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip);
     res.render('admin/pages/products/index', {
         titlePage: 'Trang sản phẩm',
         products: products,
         filterStatus: filterStatus,
         keyword: objectSearch.keyword,
+        pagination: objectPagination,
     });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -41,3 +41,25 @@ if (formSearch) {
 }
 
 /* End form search */
+
+/* Pagination */
+const buttonPagination = document.querySelectorAll('[button-pagination]');
+
+if (buttonPagination.length > 0) {
+    let url = new URL(window.location.href);
+
+    buttonPagination.forEach((button) => {
+        button.addEventListener('click', () => {
+            const page = button.getAttribute('button-pagination');
+
+            if (page) {
+                url.searchParams.set('page', page);
+            } else {
+                url.delete('page');
+            }
+            window.location.href = url.href;
+        });
+    });
+}
+
+/* End pagination */
