4b10c56cc03fa71d6048fdd4bbff47405b4549ae
Đinh Hoàng Vũ <vudh.22itb@vku.udn.vn>
2025-01-23 15:49:59 +0700

    tính năng change status many

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var methodOverride = require('method-override');
 const database = require('./config/database');
 const systemConfig = require('./config/system');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 database.connect();
@@ -10,6 +11,8 @@ const routeClient = require('./routes/client/index.route');
 
 const app = express();
 app.use(methodOverride('_method'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT;
 
 app.set('views', './views');
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -20,3 +20,70 @@ if (buttonChangeStatus.length > 0) {
 }
 
 /* End change status */
+
+/* Table checkbox multi */
+
+const checkboxMulti = document.querySelector('[table-checkbox-multi]');
+
+if (checkboxMulti) {
+    const inputCheckAll = checkboxMulti.querySelector("input[name='checkAll']");
+    const inputsId = checkboxMulti.querySelectorAll("input[name='id']");
+
+    //check All
+    inputCheckAll.addEventListener('click', () => {
+        if (inputCheckAll.checked) {
+            inputsId.forEach((inputId) => {
+                inputId.checked = true;
+            });
+        } else {
+            inputsId.forEach((inputId) => {
+                inputId.checked = false;
+            });
+        }
+    });
+
+    //checkbox từng sản phẩm
+    inputsId.forEach((inputId) => {
+        inputId.addEventListener('click', () => {
+            const countChecked = checkboxMulti.querySelectorAll(
+                "input[name='id']:checked"
+            ).length;
+
+            if (countChecked == inputsId.length) {
+                inputCheckAll.checked = true;
+            } else {
+                inputCheckAll.checked = false;
+            }
+        });
+    });
+}
+/* End table checkbox multi */
+
+/* Form change multi */
+const formChangeMulti = document.querySelector('[form-change-multi]');
+if (formChangeMulti) {
+    formChangeMulti.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const checkboxMulti = document.querySelector('[table-checkbox-multi]');
+        const inputChecked = checkboxMulti.querySelectorAll(
+            "input[name='id']:checked"
+        );
+
+        if (inputChecked.length > 0) {
+            let ids = [];
+            const inputIds = formChangeMulti.querySelector("input[name='ids']");
+
+            inputChecked.forEach((input) => {
+                const id = input.value;
+                ids.push(id);
+            });
+            inputIds.value = ids.join(', ');
+
+            formChangeMulti.submit();
+        } else {
+            alert('Vui lòng chọn ít nhất 1 bản ghi!');
+        }
+    });
+}
+/* End form change multi */
